64a600b9889db3929c17680efb5a79f1492d2dd0
maurafortino <maurafortino@gmail.com>
2019-10-05 15:30:08 -0400

    adding info to portfolio, working on adding coding project to that portion of portfolio

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -16,10 +16,27 @@ var portfolioArray = [
     }
 ];
 
-console.log(portfolioArray[0].link);
+var upennArray = [
+    {
+        img: "assets/spiderman.png",
+        description: "A role-playing game that showcases my knowledge of JQuery. In the game a user chooses a character as their hero and then chooses an enemy to defeat. Once the user defeats all enemies they win the game, if they don't defeat all enemies, they lose. Once the game is over the user has an option to play again.",
+        link: "assets/project-files/rpg/role-playing-game/rpg.html",
+        name: "Role Playing Game"
+    },
+    {
+        img:"assets/weatherly.png",
+        description: "An app that outputs various food and drink recipes based on the weather. This was a group project in which I worked on making the ajax calls for the food API.",
+        link: "assets/project-files/project-weatherly/weatherly.html",
+        name: "wEATherly"
+    },
+    {
+        img: "assets"
+    }
+]
+var buttonsArray = ["upenn", "udemy", "person"];
 var portfolioDiv= $("#portfolio");
 
-
+//populates my portfolio main page with hover cards
 for(var i = 0; i < portfolioArray.length; i++){
     var columnDiv = $("<div>");
     var contentDiv = $("<div>");
@@ -38,3 +55,36 @@ for(var i = 0; i < portfolioArray.length; i++){
     portfolioDiv.append(columnDiv);
 }
 
+function myFunction(){
+    document.getElementById("upenn").style.visibility = 'visible';
+}
+
+function showUPENN(){
+    $("#display").empty();
+    var titleRow = $("<div>");
+    var titleCol = $("<div>");
+    titleRow.addClass("row text-center bounceInLeft");
+    titleCol.addClass("col-12");
+    titleCol.html("<h4> UPENN PROJECTS </h4>");
+    titleRow.append(titleCol);
+    $("#display").append(titleRow);
+    for(var i = 0; i < upennArray.length; i++){
+    var rowDiv = $("<div>");
+    var newImg = $("<img>");
+    var imgDiv = $("<div>");
+    var descDiv = $("<div>");
+    newImg.attr("src", upennArray[i].img);
+    newImg.addClass("portfolio-image");
+    imgDiv.addClass("col-4");
+    imgDiv.append(newImg)
+    descDiv.addClass("col-8");
+    descDiv.html(upennArray[i].description + "<br><br> <a href=" + upennArray[i].link + " target='_blank'>" + upennArray[i].name + "</a>");
+    rowDiv.addClass("row py-2 fadeIn");
+    rowDiv.append(imgDiv, descDiv);
+    $("#display").append(rowDiv);
+    }
+}
+
+function showUDEMY(){
+    $("#display").empty();
+}
