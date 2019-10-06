9b75be4b7d81d4e443069331be13214c5fa2c47e
maurafortino <maurafortino@gmail.com>
2019-10-05 21:21:54 -0400

    working website

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,4 @@
+
 var portfolioArray = [
     {
         frontFace: "coding projects",
@@ -7,7 +8,7 @@ var portfolioArray = [
     {
         frontFace: "illustrations",
         backFace: "click here",
-        link:"portfolio-illustrations.html"
+        link: "portfolio-illustrations.html"
     },
     // {
     //     frontFace: "events",
@@ -21,31 +22,36 @@ var upennArray = [
         img: "assets/spiderman.png",
         description: "A role-playing game that showcases my knowledge of JQuery. In the game a user chooses a character as their hero and then chooses an enemy to defeat. Once the user defeats all enemies they win the game, if they don't defeat all enemies, they lose. Once the game is over the user has an option to play again.",
         link: "https://maurafortino.github.io/role-playing-game/",
-        name: "Role Playing Game"
+        name: "Role Playing Game",
+        alt: "an image of my spiderman role playing game"
     },
     {
-        img:"assets/weatherly.png",
+        img: "assets/weatherly.png",
         description: "An app that outputs various food and drink recipes based on the weather. This was a group project in which I worked on making the ajax calls for the food API.",
         link: "https://maurafortino.github.io/project-weatherly/",
-        name: "wEATherly"
+        name: "wEATherly",
+        alt: "an image of my group project - wEATherly"
     },
     {
         img: "assets/giphy.png",
         description: "This is a website I created using the giphy API. The theme is comedians and the user can create buttons for whichever comedian they want. We you click on a comedian button, ten gifs pop up. To animate the gifs just click, to make them stop, click again.",
         link: "https://maurafortino.github.io/giphy/",
-        name: "Giphy API"
+        name: "Giphy API",
+        alt: "an image of my giphy website"
     },
     {
         img: "assets/trivia.png",
         description: "A trivia game I created based on the popular cartoon from Nickelodeon, Avatar: The Last Airbender. The user has a certain amount of time to answer each question. To win the game, the user must get more answers correct than they get wrong.",
         link: "https://maurafortino.github.io/trivia-game/",
-        name: "ATLA Trivia Game"
+        name: "ATLA Trivia Game",
+        alt: "an image of my Avatar: The last Airbender trivia game"
     },
     {
         img: "assets/trains.png",
         description: "An app that uses firebase to store the data added through the input fields and then display that data on the schedule.",
         link: "https://maurafortino.github.io/train-times/",
-        name: "Firebase Train Schedule"
+        name: "Firebase Train Schedule",
+        alt: "an image of my train schedule website"
     }
 ];
 
@@ -54,19 +60,22 @@ var udemyArray = [
         img: "assets/colors.png",
         description: "An app that I made in my udemy course that requires the user to guess which color the rgb color code is. There is an easy and a hard mode.",
         link: "assets/project-files/color-game/colors.html",
-        name: "RGB Color Game"
+        name: "RGB Color Game",
+        alt: "an image of my color game app"
     },
     {
         img: "assets/todo.png",
         description: "A to do list created with jquery for my udemy course. The user can input their own to do list and also delete each item as they complete each task.",
-        link:"assets/project-files/to-do-list/jqueryapp.html",
-        name: "To Do List"
+        link: "assets/project-files/to-do-list/jqueryapp.html",
+        name: "To Do List",
+        alt: "an image of my to do list app"
     },
     {
         img: "assets/museum.png",
         description: "A fake candy museum home page which I created solely with html and css. I also decided to practice my Spanish and wrote the whole copy content in Spanish.",
         link: "assets/project-files/museum/Museum.html",
-        name: "El Museo del Dulce"
+        name: "El Museo del Dulce",
+        alt: "an image of my candy museum home page"
     }
 ];
 
@@ -75,19 +84,73 @@ var personalArray = [
         img: "assets/first.png",
         description: "While I would prefer not to showcase this website as it's definitely not my best work. It's important to remember where you started. This is the first website I ever made and as you'll see it's... not great.",
         link: "assets/project-files/first-website/former.html",
-        name: "First Website"
+        name: "First Website",
+        alt: "an image of the home page of my first website"
     },
     {
         img: "assets/second.png",
         description: "This was the redesign of my first website. While it's not the greatest, it's also a lot better than the first one I did. This one is actually responsive and fits most screen sizes.",
         link: "assets/project-files/second-website/home.html",
-        name: "Second Website"
+        name: "Second Website",
+        alt: "an image of the home page of my second website"
     }
 ];
-var portfolioDiv= $("#portfolio");
+var popArray = [
+    {
+        link: "assets/illustrations/StrangerThings1.png",
+        description: "I created this illustration after the second season of Stranger Things. After the season, many people joked that Steve Harrington acted like a proud parent so I combined that with the popular meme 'Don't Talk to Me and My Son ever again.'",
+        sold: "6 stickers",
+        redbubble: "https://www.redbubble.com/people/maurafortino/works/28977051-stranger-things?asc=t",
+        alt: "a black circle with a drawing of steve harrington and dustin from Stranger Things with the quote 'Don't talk to me or my son, ever again'"
+    },
+    {
+        link: "assets/illustrations/b99-quote.jpg",
+        description: "Brooklyn 99 is hands down one of my favorite shows. This is a quote by Captain Holt from the fifth season and it really stuck with me. While the show is a comedy, it also does include some very touching moments.",
+        sold: "100 various products (my best seller!)",
+        redbubble: "https://www.redbubble.com/people/maurafortino/works/29717971-brooklyn-99?asc=t",
+        alt: "a quote that says 'Everytime someone steps up and says who they are, the world becomes a better, more interesting place.'"
+    },
+    {
+        link: "assets/illustrations/the-aces.jpg",
+        description: "The aces are one of my favorite bands. They're an all girl group from Utah and seeing as their name is 'The Aces' I was inspired to draw each of them as an Ace from a deck of cards.",
+        sold: "40 various products",
+        redbubble: "https://www.redbubble.com/people/maurafortino/works/33051842-the-aces?asc=t",
+        alt: "A minimalistic drawing of the members of The Aces each on their own Ace from a deck of cards"
+    },
+    {
+        link: "assets/illustrations/Shrek.jpg",
+        description: "Shrek is a cinemactic masterpiece. I created this illustration to honor my love for the movie and puns.",
+        sold: "9 various products",
+        redbubble: "https://www.redbubble.com/people/maurafortino/works/29897580-shrek?asc=t",
+        alt: "A drawing of Shrek that says 'I'm OGRE the moon for you, babe'"
+    },
+    {
+        link: "assets/illustrations/JVN.jpg",
+        description: "Queer Eye's Johnathan Van Ness is easily one of the most positivie people. He has some of the best quotes and I decided to pay homage to them with this illustration.",
+        sold: "17 various products",
+        redbubble: "https://www.redbubble.com/people/maurafortino/works/35371505-jonathan-van-ness-quotes?asc=t",
+        alt: "A minimalstic drawing of Jonthan Van Ness with his quotes surrounding him in rainbow colors"
+    }
+];
+
+var scArray = [
+    {
+        link: "assets/illustrations/sc-mummin.png",
+        description: "My parents host a New Years Day part every year for the Mummer's Parade. In 2018, I created two snapchat filters for the party. This is one of them.",
+        alt: "An image of my friend and I on Mummer's Day with a Mummer's Day snapchat filter that says 'Mummin is a habit'"
+    },
+    {
+        link: "assets/illustrations/sc-mummers-day.png",
+        description: "This was the second option for the 2018 Mummer's Day Parade filters.",
+        alt: "An image of my sister and her three friends on Mummer's Day with a Mummer's Day snapchat filter that says 'The First with the Fortions, We got it from our Mummer'"
+    }
+
+];
+var portfolioDiv = $("#portfolio");
+var illustrationsDiv = $("#illustrations");
 
 //populates my portfolio main page with hover cards
-for(var i = 0; i < portfolioArray.length; i++){
+for (var i = 0; i < portfolioArray.length; i++) {
     var columnDiv = $("<div>");
     var contentDiv = $("<div>");
     var cardDiv = $("<div>");
@@ -105,11 +168,11 @@ for(var i = 0; i < portfolioArray.length; i++){
     portfolioDiv.append(columnDiv);
 };
 
-function myFunction(){
+function myFunction() {
     document.getElementById("upenn").style.visibility = 'visible';
 };
 
-function showUPENN(){
+function showUPENN() {
     $("#display").empty();
     var titleRow = $("<div>");
     var titleCol = $("<div>");
@@ -118,24 +181,25 @@ function showUPENN(){
     titleCol.html("<h4> UPENN PROJECTS </h4>");
     titleRow.append(titleCol);
     $("#display").append(titleRow);
-    for(var i = 0; i < upennArray.length; i++){
-    var rowDiv = $("<div>");
-    var newImg = $("<img>");
-    var imgDiv = $("<div>");
-    var descDiv = $("<div>");
-    newImg.attr("src", upennArray[i].img);
-    newImg.addClass("portfolio-image");
-    imgDiv.addClass("col-4");
-    imgDiv.append(newImg)
-    descDiv.addClass("col-8");
-    descDiv.html(upennArray[i].description + "<br><br> <a href=" + upennArray[i].link + " target='_blank'>" + upennArray[i].name + "</a>");
-    rowDiv.addClass("row py-2 fadeIn");
-    rowDiv.append(imgDiv, descDiv);
-    $("#display").append(rowDiv);
+    for (var i = 0; i < upennArray.length; i++) {
+        var rowDiv = $("<div>");
+        var newImg = $("<img>");
+        var imgDiv = $("<div>");
+        var descDiv = $("<div>");
+        newImg.attr("src", upennArray[i].img);
+        newImg.attr("alt", upennArray[i].alt);
+        newImg.addClass("portfolio-image");
+        imgDiv.addClass("col-4");
+        imgDiv.append(newImg)
+        descDiv.addClass("col-8");
+        descDiv.html(upennArray[i].description + "<br><br> <a href=" + upennArray[i].link + " target='_blank'>" + upennArray[i].name + "</a>");
+        rowDiv.addClass("row py-2 fadeIn");
+        rowDiv.append(imgDiv, descDiv);
+        $("#display").append(rowDiv);
     };
 };
 
-function showUDEMY(){
+function showUDEMY() {
     $("#display").empty();
     var titleRow = $("<div>");
     var titleCol = $("<div>");
@@ -144,24 +208,25 @@ function showUDEMY(){
     titleCol.html("<h4> UDEMY PROJECTS </h4>");
     titleRow.append(titleCol);
     $("#display").append(titleRow);
-    for(var i =0; i < udemyArray.length; i ++){
-    var rowDiv = $("<div>");
-    var newImg = $("<img>");
-    var imgDiv = $("<div>");
-    var descDiv = $("<div>");
-    newImg.attr("src", udemyArray[i].img);
-    newImg.addClass("portfolio-image");
-    imgDiv.addClass("col-4");
-    imgDiv.append(newImg)
-    descDiv.addClass("col-8");
-    descDiv.html(udemyArray[i].description + "<br><br> <a href=" + udemyArray[i].link + " target='_blank'>" + udemyArray[i].name + "</a>");
-    rowDiv.addClass("row py-2 fadeIn");
-    rowDiv.append(imgDiv, descDiv);
-    $("#display").append(rowDiv);
+    for (var i = 0; i < udemyArray.length; i++) {
+        var rowDiv = $("<div>");
+        var newImg = $("<img>");
+        var imgDiv = $("<div>");
+        var descDiv = $("<div>");
+        newImg.attr("src", udemyArray[i].img);
+        newImg.attr("alt", udemyArray[i].alt);
+        newImg.addClass("portfolio-image");
+        imgDiv.addClass("col-4");
+        imgDiv.append(newImg)
+        descDiv.addClass("col-8");
+        descDiv.html(udemyArray[i].description + "<br><br> <a href=" + udemyArray[i].link + " target='_blank'>" + udemyArray[i].name + "</a>");
+        rowDiv.addClass("row py-2 fadeIn");
+        rowDiv.append(imgDiv, descDiv);
+        $("#display").append(rowDiv);
     };
 };
 
-function showPers(){
+function showPers() {
     $("#display").empty();
     var titleRow = $("<div>");
     var titleCol = $("<div>");
@@ -170,19 +235,74 @@ function showPers(){
     titleCol.html("<h4> PERSONAL PROJECTS </h4>");
     titleRow.append(titleCol);
     $("#display").append(titleRow);
-    for(var i =0; i < personalArray.length; i ++){
-    var rowDiv = $("<div>");
-    var newImg = $("<img>");
-    var imgDiv = $("<div>");
-    var descDiv = $("<div>");
-    newImg.attr("src", personalArray[i].img);
-    newImg.addClass("portfolio-image");
-    imgDiv.addClass("col-4");
-    imgDiv.append(newImg)
-    descDiv.addClass("col-8");
-    descDiv.html(personalArray[i].description + "<br><br> <a href=" + personalArray[i].link + " target='_blank'>" + personalArray[i].name + "</a>");
-    rowDiv.addClass("row py-2 fadeIn");
-    rowDiv.append(imgDiv, descDiv);
-    $("#display").append(rowDiv);
+    for (var i = 0; i < personalArray.length; i++) {
+        var rowDiv = $("<div>");
+        var newImg = $("<img>");
+        var imgDiv = $("<div>");
+        var descDiv = $("<div>");
+        newImg.attr("src", personalArray[i].img);
+        newImg.attr("alt", personalArray[i].alt);
+        newImg.addClass("portfolio-image");
+        imgDiv.addClass("col-4");
+        imgDiv.append(newImg)
+        descDiv.addClass("col-8");
+        descDiv.html(personalArray[i].description + "<br><br> <a href=" + personalArray[i].link + " target='_blank'>" + personalArray[i].name + "</a>");
+        rowDiv.addClass("row py-2 fadeIn");
+        rowDiv.append(imgDiv, descDiv);
+        $("#display").append(rowDiv);
+    };
+};
+
+function showPop() {
+    illustrationsDiv.empty();
+    var titleRow = $("<div>");
+    var titleCol = $("<div>");
+    titleRow.addClass("row text-center bounceInLeft");
+    titleCol.addClass("col-12");
+    titleCol.html("<h4> POP CULTURE ILLUSTRATIONS </h4>");
+    titleRow.append(titleCol);
+    illustrationsDiv.append(titleRow);
+    for (var i = 0; i < popArray.length; i++) {
+        var rowDiv = $("<div>");
+        var imgCol = $("<div>");
+        var contentCol = $("<div>");
+        var newImg = $("<img>");
+        rowDiv.addClass("row py-3 fadeIn");
+        imgCol.addClass("col-4");
+        newImg.attr("src", popArray[i].link);
+        newImg.attr("alt", popArray[i].alt);
+        newImg.addClass("portfolio-image");
+        imgCol.append(newImg);
+        contentCol.addClass("col-8");
+        contentCol.html(popArray[i].description + "<br><br> sold:" + popArray[i].sold + "<br><br> <a href=" + popArray[i].redbubble + "target='_blank'> buy here! </a>")
+        rowDiv.append(imgCol, contentCol);
+        illustrationsDiv.append(rowDiv);
+    };
+};
+
+function showSC() {
+    illustrationsDiv.empty();
+    var titleRow = $("<div>");
+    var titleCol = $("<div>");
+    titleRow.addClass("row text-center bounceInLeft");
+    titleCol.addClass("col-12");
+    titleCol.html("<h4> SNAPCHAT FILTERS </h4>");
+    titleRow.append(titleCol);
+    illustrationsDiv.append(titleRow);
+    for (var i = 0; i < scArray.length; i++) {
+        var rowDiv = $("<div>");
+        var imgCol = $("<div>");
+        var contentCol = $("<div>");
+        var newImg = $("<img>");
+        rowDiv.addClass("row py-3 fadeIn");
+        imgCol.addClass("col-4");
+        newImg.attr("src", scArray[i].link);
+        newImg.attr("alt", scArray[i].alt);
+        newImg.addClass("portfolio-image");
+        imgCol.append(newImg);
+        contentCol.addClass("col-8");
+        contentCol.text(scArray[i].description);
+        rowDiv.append(imgCol, contentCol);
+        illustrationsDiv.append(rowDiv);
     };
 };
